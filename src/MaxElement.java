ca8ba7cf5032b84bea64f06e57296d6502d63c7d
unknown <ramasatya19122000@gmail.com>
2025-07-13 17:44:34 +0530

    Greater Number file added sop added

--- src/MaxElement.java
+++ src/MaxElement.java
@@ -7,5 +7,6 @@ public class MaxElement {
         List<Integer> nums= Arrays.asList(1,20,8,14,9,7);
         int max=nums.stream().distinct().sorted(Comparator.reverseOrder()).findFirst().get();
         System.out.println(max);
+        System.out.println(nums);
     }
 }
